e7cfa820b6d80494a018b614ef786782f654585c
Helge Heß <me@helgehess.eu>
2014-07-12 14:28:55 +0200

    Empty project.

---
+++ SwiftyExpat/SwiftyExpat.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyExpat.h
+//  SwiftyExpat
+//
+//  Created by Helge Heß on 7/12/14.
+//  Copyright (c) 2014 Always Right Institute. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyExpat.
+FOUNDATION_EXPORT double SwiftyExpatVersionNumber;
+
+//! Project version string for SwiftyExpat.
+FOUNDATION_EXPORT const unsigned char SwiftyExpatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyExpat/PublicHeader.h>
+
+
