c91c8a79595ae7dfc4a294fae7c4832ef627471c
Helge Heß <me@helgehess.eu>
2014-07-13 15:45:05 +0200

    create parser, but no CB funcs.

--- SwiftyExpat/main.swift
+++ SwiftyExpat/main.swift
@@ -8,8 +8,31 @@
 
 import Foundation
 
+/*
+typedef void (XMLCALL *XML_StartElementHandler) (void *userData,
+  const XML_Char *name,
+  const XML_Char **atts);
+*/
+func startCB(ud: COpaquePointer, name: CString, attrs: UnsafePointer<CString>) {
+  
+}
+
+/*
+typedef void (XMLCALL *XML_EndElementHandler) (void *userData,
+const XML_Char *name);
+*/
+func endCB(ud: COpaquePointer, name: CString) {
+  
+}
+
 func testit() {
   println("testing it ...")
   
-  XML_ParserCreateNS(CString("UTF-8"), 58 /* ':' */)
+  var p : XML_Parser = nil
+  
+  "UTF-8".withCString { cs in
+    p = XML_ParserCreateNS(cs, 58 /* ':' */)
+  }
+  
+  // XML_SetElementHandler(p, startCB, endCB)
 }
