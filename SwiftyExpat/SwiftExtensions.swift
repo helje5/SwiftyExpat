2b90a9c28d0e89170497b2bc063029d879f4e54f
Helge Hess <me@helgehess.eu>
2014-07-15 17:23:55 +0200

    Added a README and CDATA callback.

---
+++ SwiftyExpat/SwiftExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftExtensions.swift
+//  SwiftyExpat
+//
+//  Created by Helge Heß on 7/15/14.
+//  Copyright (c) 2014 Always Right Institute. All rights reserved.
+//
+
+import Foundation
