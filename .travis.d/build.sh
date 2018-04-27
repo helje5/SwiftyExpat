0b31a91f7a820ce311e5b12a28602da3fb3f0171
Helge Hess <helge@alwaysrightinstitute.com>
2018-04-27 13:49:24 +0200

    Travis: Do not build SPM on Xcode 7.3
    ... the SPM setup requires Swift 4.

--- .travis.d/build.sh
+++ .travis.d/build.sh
@@ -3,21 +3,10 @@
 if [[ "$TRAVIS_OS_NAME" == "Linux" ]]; then
   swift build -c release
   swift build -c debug
-else
-  env
-
-  SWIFT_VERSION="$(${SWIFT_DRIVER} --version | head -1 | sed 's/^.*[Vv]ersion[\t ]*\([.[:digit:]]*\).*$/\1/g')"
-  declare -a SWIFT_VERSION_LIST="(${SWIFT_VERSION//./ })"
-  SWIFT_MAJOR=${SWIFT_VERSION_LIST[0]}
-  SWIFT_MINOR=${SWIFT_VERSION_LIST[1]}
-  SWIFT_SUBMINOR_OPT=${SWIFT_VERSION_LIST[2]}
-  SWIFT_SUBMINOR=${SWIFT_SUBMINOR_OPT}
-  if [[ "x${SWIFT_SUBMINOR}" = "x" ]]; then SWIFT_SUBMINOR=0; fi
-  
-  echo "SWIFT MAJOR: ${SWIFT_MAJOR}"
-
+elif [[ "$TRAVIS_OSX_IMAGE" == "xcode7.3" ]]; then
   xcodebuild
-  
+else
+  xcodebuild
   swift build -c release
   swift build -c debug
 fi
